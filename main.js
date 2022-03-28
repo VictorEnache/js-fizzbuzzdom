a5c8ccd0a2690abdcdeb0237ac1224291a978fdc
VictorEnache <victor22enache@gmail.com>
2022-03-28 16:21:51 +0200

    cambio colore in base al contenuto

--- main.js
+++ main.js
@@ -9,13 +9,16 @@ for (let i = 1; i <= 100; i++) {
 
     if(i % 3 == 0 && i % 5 == 0) {
         listItem.innerHTML = `<span>FizzBuzz</span>`
+        listItem.style.backgroundColor = "#f0466f"
     }
     else if (i % 3 == 0) {
         listItem.innerHTML = `<span>Fizz</span>`
+        listItem.style.backgroundColor = "#0ad6a1"
     }
 
     else if (i % 5 == 0) {
         listItem.innerHTML = `<span>Buzz</span>`
+        listItem.style.backgroundColor = "#ffd166"
     }
 
     console.log(listItem.innerHTML)
